46667d4349ed84c3763f3d480766f70adb0cbb0b
flyzgq <zgqtsyx@163.com>
2024-07-03 23:23:03 +0800

    调整测试类

---
+++ src/test/java/com/alibaba/cloud/ai/example/tongyi/TongYiApplicationTest.java
@@ -0,0 +1,13 @@
+package com.alibaba.cloud.ai.example.tongyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongYiApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
