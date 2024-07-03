4f00cd2ac611cb0e611b9ede21c7a6629b8948ba
flyzgq <zgqtsyx@163.com>
2024-07-03 23:02:30 +0800

    初版代码

---
+++ src/test/java/com/example/alibaspringaidemo/AlibaSpringAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alibaspringaidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibaSpringAiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
